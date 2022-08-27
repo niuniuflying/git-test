40bbec7602e7e4bf7d28943af4e6c88c420eff1c
niuniuflying <108665575+niuniuflying@users.noreply.github.com>
2022-08-27 23:29:54 +0800

    pull

--- src/main/java/git.java
+++ src/main/java/git.java
@@ -8,5 +8,6 @@ public class git {
         System.out.println("hot-fix test...");
         System.out.println("push test...");
         System.out.println("push test...");
+        System.out.println("pull test...");
     }
 }
