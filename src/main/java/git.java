859f29c9361a4e142cbb2844c15de4572b8e4cf5
niuniuflying <84273140@qq.com>
2022-08-27 23:23:06 +0800

    push commit

--- src/main/java/git.java
+++ src/main/java/git.java
@@ -6,5 +6,6 @@ public class git {
         System.out.println("git 4...");
         System.out.println("master test...");
         System.out.println("hot-fix test...");
+        System.out.println("push test...");
     }
 }
